397185e52b7e7c53f152d25b8540063a24e943ed
Flavio Garrido <ing.flaviogarrido@gmail.com>
2022-04-11 17:57:10 -0300

    decorators examples

---
+++ Decorators/decorator_func_stacking.py
@@ -0,0 +1,36 @@
+def big_container(collective_material):
+    def wrapper(our_function):
+        def internal_wrapper(*args):
+            our_function(*args)
+            print('<strong>*</strong> The whole order would be packed with', collective_material)
+            print()
+        return internal_wrapper
+    return wrapper
+
+def warehouse_decorator(material):
+    def wrapper(our_function):
+        def internal_wrapper(*args):
+            our_function(*args)
+            print('<strong>*</strong> Wrapping items from {} with {}'.format(our_function.__name__, material))
+        return internal_wrapper
+    return wrapper
+
+@big_container('plain cardboard')
+@warehouse_decorator('bubble foil')
+def pack_books(*args):
+    print("We'll pack books:", args)
+
+@big_container('colourful cardboard')
+@warehouse_decorator('foil')
+def pack_toys(*args):
+    print("We'll pack toys:", args)
+
+@big_container('strong cardboard')
+@warehouse_decorator('cardboard')
+def pack_fruits(*args):
+    print("We'll pack fruits:", args)
+
+
+pack_books('Alice in Wonderland', 'Winnie the Pooh')
+pack_toys('doll', 'car')
+pack_fruits('plum', 'pear')
